0ff6b029f15a5e8eb5a89aef8e1941ab055ed813
Andrii Hutnyk <andreigutnik@gmail.com>
2023-05-31 11:54:26 +0200

    Update hw-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainer = document.querySelector("ul.gallery");
+const gallaryItemMarkup = createGallaryItemMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", gallaryItemMarkup);
+
+galleryContainer.addEventListener("click", ongalleryContainerClick);
+
+function createGallaryItemMarkup(galleryItems) {
+	return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+		<li class="gallery__item">
+			<a class="gallery__link" href="${original}">
+				<img
+					class="gallery__image"
+					src="${preview}"
+					data-source="${original}"
+					alt="${description}"
+				/>
+			</a>
+		</li>
+		`;
+    })
+    .join("");
+}
+
+function ongalleryContainerClick(evt) {
+  // Canceling standard actions
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  const originalImage = evt.target.dataset.source;
+  // Open the gallary
+  const instance = basicLightbox.create(`
+    <img src="${originalImage}" width="800" height="600">
+`);
+  instance.show();
+  // Close the gallary
+  galleryContainer.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,64 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryContainer = document.querySelector("ul.gallery");
+const gallaryItemMarkup = createGallaryItemMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", gallaryItemMarkup);
+
+galleryContainer.addEventListener("click", ongalleryContainerClick);
+
+function createGallaryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+			return `
+			<li class="gallery__item">
+				<a class="gallery__link" href="${original}">
+				<img class="gallery__image" src="${preview}" alt="${description}"/>
+				</a>
+			</li>
+			`;
+			})
+    .join("");
+}
+
+function ongalleryContainerClick(evt) {
+  //console.log(evt.target)
+	// Canceling standard actions
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  // Open the gallary
+  const gallery = new SimpleLightbox(".gallery__item a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+	}); 
+	
+  // Close the gallary
+  galleryContainer.addEventListener("keydown", (evt) => {
+    if (evt.code === "Escape") {
+			gallery.refresh();
+    }
+	});
+	gallery.on("closed.simplelightbox", () => {
+    gallery.refresh();
+  });  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
